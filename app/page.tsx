27007241825ccd1f34f486c96581c567fbfbd455
PieterSmith81 <109296884+PieterSmith81@users.noreply.github.com>
2025-01-02 17:09:17 +0000

    Light and dark background colours updated for the information sections on the landing page. Sign up confirmation message updated to tell the user to check their spam folder if the email doesn't arrive shortly

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Index() {
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4 max-w-[690px] mx-auto">
         <div className="grid grid-cols-1 gap-8">
-          <div className="flex flex-col items-center p-6 rounded-lg bg-[#9CE8DF80] dark:bg-[#BF941280]">
+          <div className="flex flex-col items-center p-6 rounded-lg bg-[#C7D6E380] dark:bg-[#191B4480]">
             <h2 className="text-xl font-normal mb-4">
               Discover <strong>local</strong> connections centered around your
               unique interests
@@ -19,7 +19,7 @@ export default async function Index() {
               className="w-full h-auto"
             />
           </div>
-          <div className="flex flex-col items-center p-6 rounded-lg bg-[#FFE8A380] dark:bg-[#3AB9AA80]">
+          <div className="flex flex-col items-center p-6 rounded-lg bg-[#F9DCAF80] dark:bg-[#3A2A1C80]">
             <h2 className="text-xl font-normal mb-4">
               Find your tribe within walking distance, united by{" "}
               <strong>common</strong> pursuits
@@ -32,7 +32,7 @@ export default async function Index() {
               className="w-full h-auto"
             />
           </div>
-          <div className="flex flex-col items-center p-6 rounded-lg bg-[#9CF3C180] dark:bg-[#19197080]">
+          <div className="flex flex-col items-center p-6 rounded-lg bg-[#E6E3BD80] dark:bg-[#17363980]">
             <h2 className="text-xl font-normal mb-4">
               Experience a <strong>secure</strong> environment where you can
               comfortably explore new friendships
